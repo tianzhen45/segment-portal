5488428ed105d92eb0e8dec870a355e92ee70f41
tianzhen <504618712@qq.com>
2024-05-20 15:17:00 +0800

    初始化提交（vue-cli webpack基本环境，element-ui，vue-clipboard，SegForm组件）

---
+++ src/components/Demo.vue
@@ -0,0 +1,20 @@
+<template>
+ <div id="app">
+    <el-button @click="visible = true">Button</el-button>
+    <el-dialog :visible.sync="visible" title="Hello world">
+      <p>Try Element</p>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo',
+  data () {
+    return {
+      msg: 'Demo component',
+      visible: false
+    }
+  }
+}
+</script>
---
+++ src/components/SegForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-form ref="form" :model="form" label-width="80px">
+    <el-form-item label="分词内容">
+      <el-input type="textarea" v-model="form.data" autosize placeholder="请输入需要分词的中文内容，多条内容使用换行分隔"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">执行 <i class="el-icon-check"></i></el-button>
+      <el-button type="info" @click="onCopy">复制 <i class="el-icon-share"></i></el-button>
+      <el-button type="warning" @click="onClear">清空 <i class="el-icon-delete"></i></el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  name: 'SegForm',
+  data () {
+    return {
+      form: {
+        data: ''
+      },
+      resultData: '开户_日期\tDPST_DT\n开户_机构编号\tDPST_INS_NO'
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+      this.form.data = this.resultData
+    },
+    onCopy () {
+      this.$copyText(this.form.data)
+      this.$message('复制成功！直接粘贴到Excel中自动适应格式')
+    },
+    onClear () {
+      this.form.data = ''
+    }
+  }
+}
+</script>
